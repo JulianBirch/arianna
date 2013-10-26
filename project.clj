a59b2b44bdbabbb77a3923e78d0f486b71bbf004
Julian Birch <julian.birch@gmail.com>
2013-10-26 10:57:08 +0100

    Setup codox.

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[spyscope "0.1.3"]
-                 [org.clojure/core.typed "0.2.13"]
                  [net.colourcoding/poppea "0.1.9"]
                  [potemkin "0.3.3"]
                  [stencil "0.3.2"]]
   :core.typed {:check [arianna]}
+  :codox {:include [arianna]}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/tools.namespace "0.2.3"]]
                    :source-paths ["dev"]}
